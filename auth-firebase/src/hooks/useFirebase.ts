e546da5d90eca88b89a66fa3eaa72c576440efe8
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-03-15 23:22:41 +0100

    feat: login works + redirect to login

--- auth-firebase/src/hooks/useFirebase.ts
+++ auth-firebase/src/hooks/useFirebase.ts
@@ -5,30 +5,30 @@ import { FirebaseApp, initializeApp } from 'firebase/app';
 // TODO: initialize firebase auth
 // require('firebase/auth');
 
-import { Auth, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, UserCredential, } from 'firebase/auth';
-import { useState } from 'react';
+import { Auth, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, UserCredential, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 import { Link, useNavigate } from "react-router-dom";
 
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_apiKey,
-//     authDomain: import.meta.env.VITE_authDomain,
-//     projectId: import.meta.env.VITE_projectId,
-//     storageBucket: import.meta.env.VITE_storageBucket,
-//     messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//     appId: import.meta.env.VITE_appId,
-//     measurementId: import.meta.env.VITE_measurementId,
-// }
-
 const firebaseConfig = {
-    apiKey: "AIzaSyD72De_JkY5tzIL89dSPvZwmLmwTCUn58Y",
-    authDomain: "authentication-3752d.firebaseapp.com",
-    projectId: "authentication-3752d",
-    storageBucket: "authentication-3752d.appspot.com",
-    messagingSenderId: "491111340973",
-    appId: "1:491111340973:web:6889a9f0fc9b029612df24",
-    measurementId: "G-H7L9ZR9LCH"
-  };
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
+    measurementId: import.meta.env.VITE_measurementId,
+}
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD72De_JkY5tzIL89dSPvZwmLmwTCUn58Y",
+//     authDomain: "authentication-3752d.firebaseapp.com",
+//     projectId: "authentication-3752d",
+//     storageBucket: "authentication-3752d.appspot.com",
+//     messagingSenderId: "491111340973",
+//     appId: "1:491111340973:web:6889a9f0fc9b029612df24",
+//     measurementId: "G-H7L9ZR9LCH"
+//   };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
 const auth: Auth = getAuth(app);
@@ -39,15 +39,26 @@ console.log({ app })
 export default () => {
     const navigate = useNavigate();
 
-    const login = (email: string, password: string) => {
-        return signInWithEmailAndPassword(auth, email, password)
+
+    const login = (emailLogin: string, password: string) => {
+        return signInWithEmailAndPassword(auth, emailLogin, password)
         .then((userCredential: UserCredential) => {
             // Signed in
             const user = userCredential.user;
-            console.log(user)
+
             // redirect to profile
-            // navigate('/', {replace: true} );
-            return user
+            let email = user.email;
+            let uid = user.uid;
+            let lastLoginAt = user.metadata.lastSignInTime;
+            let createdAt = user.metadata.creationTime;
+            let token = ''
+            user.getIdToken().then((idToken) => {
+                token = idToken.toString();
+                navigate('/', {state: {email,uid,lastLoginAt,createdAt,token} });
+            }).catch((error) => {
+                console.log(error)
+                navigate('/', {state: {email,uid,lastLoginAt,createdAt,token} });
+            });
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -58,14 +69,23 @@ export default () => {
     }
 
     const register = (nickname: string, email: string, password: string) => {
-        console.log({ nickname, email, password })
         return createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential: UserCredential) => {
             const user = userCredential.user;
-            console.log(user)
+
             // redirect to profile
-            // navigate('/', {replace: true} );
-            return user
+            let email = user.email;
+            let uid = user.uid;
+            let lastLoginAt = user.metadata.lastSignInTime;
+            let createdAt = user.metadata.creationTime;
+            let token = ''
+            user.getIdToken().then((idToken) => {
+                token = idToken.toString();
+                navigate('/', {state: {email,uid,lastLoginAt,createdAt,token} });
+            }).catch((error) => {
+                console.log(error)
+                navigate('/', {state: {email,uid,lastLoginAt,createdAt,token} });
+            });
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -75,10 +95,20 @@ export default () => {
         });
     }
 
+    const logout = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            navigate('/login')
+        }).catch((error) => {
+            // An error happened.
+            console.log(error)
+        });
+    }
+
     return{
         login,
         register,
-        // - logout
+        logout,
         // - reset password
     }
 }
--- auth-firebase/src/screens/Profile.tsx
+++ auth-firebase/src/screens/Profile.tsx
@@ -1,9 +1,26 @@
 import { Key } from "lucide-react";
 
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
+
+import useFirebase from "../hooks/useFirebase";
+
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+
 
 
 export default () => {
+    const navigate = useNavigate();
+
+    const { state } = useLocation();
+    const { email, uid, lastLoginAt, createdAt, token  } = state || {};
+    console.log(state)
+    
+    //redirect to login if not logged in
+    useEffect(() => {
+        if(!state) navigate('/auth/login');
+    }, [])
+
     return(
         <div className='w-10/12 xsm:w-96 md:w-128 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
             <div className="flex items-center mb-2">
@@ -12,15 +29,16 @@ export default () => {
             </div>
             <div className="mb-2">
                 <p className="inline">You are logged in with the emailaddress </p>
-                <p className="inline"><b>Tibo.messiaen@howest.be</b>.</p>
+                <p className="inline"><b>{email}</b>.</p>
             </div>
             <div className="mb-2">
-                <p>The uid is 0BfzxfqCXsbqz61CGZBQpI1khEml.</p>
-                <p>Last login: Mon, 21 Mar 2022 08:55:06 GMT</p>
+                <p>The uid is {uid}.</p>
+                <p>Last login: {lastLoginAt}</p>
+                <p>Created at: {createdAt}</p>
             </div>
             <div className="mb-2">
                 <p>Token to access bakcend services:</p>
-                <p className="overflow-x-scroll blur-sm hover:blur-none"><b>0BfzxfqCXsbqz61CGZBQpI1khEml0BfzxfqCXsbqz61CGZBQpI1khEml0BfzxfqCXsbqz61CGZBQpI1khEml</b></p>
+                <p className="overflow-x-scroll blur-sm hover:blur-none whitespace-nowrap"><b>{token}</b></p>
             </div>
             <Link className="text-end p-1" to={'/auth/login'}><p>Are you dont here? Don't forget to logout. <b className="bg-blue-500 w-full p-2 rounded-md text-white mb-2">Logout</b></p></Link>
         </div>
